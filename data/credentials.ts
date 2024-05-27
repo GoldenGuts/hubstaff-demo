4746016df401b57334a3ef94b2965f86cac3c71e
Jyotirmay <51854855+GoldenGuts@users.noreply.github.com>
2024-05-27 13:38:36 +0530

    clean-up

--- data/credentials.ts
+++
@@ -1,5 +0,0 @@
-export const creds = {
-  username: process.env.CUSTOMER_USERNAME || "please_provide_username",
-  password: process.env.CUSTOMER_PASSWORD || "please_provide_password",
-  name: "Jyotirmay Swarnkar",
-};
